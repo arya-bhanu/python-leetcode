9a75b81d868765a99375c01b611dcdad06d7dc4a
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-22 22:21:03 +0700

    accepted: with naive approach own solution

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("This is (main) first python program for leetcode")
+from typing import List
+
+# use naive approach
+
+
+def buildArray(nums: List[int]) -> List[int]:
+    container = []
+    for i in range(len(nums)):
+        container.append(nums[nums[i]])
+    return container
+
+
+print(buildArray([0, 2, 1, 5, 3, 4]))
