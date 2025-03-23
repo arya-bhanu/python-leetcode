bf306b2b41e762f07298c3fb0cc111bb350fe447
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 11:10:18 +0700

    accepted: approach using more simple python built-in function (startsWith and endsWith)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-# naive approach using substring principle
+# approach using more simple python built-in function (startsWith and endsWith)
 
 
 class Solution:
@@ -9,17 +9,10 @@ class Solution:
         length = len(words)
         for i in range(length):
             for j in range(i + 1, length):
-                if self.isPrefixAndSuffix(words[i], words[j]):
+                if words[j].startswith(words[i]) and words[j].endswith(words[i]):
                     counter += 1
         return counter
 
-    def isPrefixAndSuffix(self, word1, word2):
-        if len(word1) > len(word2):
-            return False
-        pref = len(word2) - len(word1)
-        return True if word2[:len(
-            word1)] == word1 and word2[pref:] == word1 else False
-
 
 s = Solution()
-print(s.countPrefixSuffixPairs(["bb", "bb"]))
+print(s.countPrefixSuffixPairs(["a", "aba", "ababa", "aa"]))
