4a7df449d729bf1151c81d6b654502ec65604d85
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 12:05:40 +0700

    accepted: count digits with convert into string

--- main.py
+++ main.py
@@ -3,21 +3,12 @@
 from typing import List
 
 
-# count digits length without convert into strings
+# count digits with converting into string
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
         counter = 0
         for num in nums:
-            if self.countDigitNumber(num) % 2 == 0:
+            if len(str(num)) % 2 == 0:
                 counter += 1
         return counter
-
-    def countDigitNumber(self, n) -> int:
-        if n == 0:
-            return 1
-        length = 0
-        while n != 0:
-            n = n // 10
-            length += 1
-        return length
