1b6687551e354e69700bc6aab6b5fe3733c77b7d
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 08:29:15 +0700

    accepted: with my own solution

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("This is (main) first python program for leetcode")
+from typing import List
+
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxNum = max(candies)
+        return [True if extraCandies + great >= maxNum else False for great in candies]
+
+
+sol = Solution()
+print(sol.kidsWithCandies([12, 1, 12], 3))
