96dce018cd7e37cb4c407a9b1ce5cc836923740b
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 08:46:59 +0700

    accepted: with my own solution

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("This is (main) first python program for leetcode")
+from typing import List
+
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        start = 0
+        for i in range(len(gain)):
+            gain[i] = start + gain[i]
+            start = gain[i]
+        maximum = max(gain)
+        return 0 if maximum < 0 else maximum
+
+
+sol = Solution()
+print(sol.largestAltitude([-5, 1, 5, 0, -7]))
+print(sol.largestAltitude([-4, -3, -2, -1, 4, 3, 2]))
