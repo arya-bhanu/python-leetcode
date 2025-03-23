0b8e0f43b659ff0ec79fa1f3b8cbb94a7fe60a8f
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 07:49:36 +0700

    approved: with naive approach -- own

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("This is (main) first python program for leetcode")
+from typing import List
+
+
+# naive approach like other language such: java, js
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max = 0
+        for lists in accounts:
+            counter = 0
+            for w in lists:
+                counter += w
+            max = counter if counter > max else max
+        return max
+
+
+sol = Solution()
+print(sol.maximumWealth([[1, 5], [7, 3], [3, 5]]))
