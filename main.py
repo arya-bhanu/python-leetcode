eac86ccd5201a44bb9073f182af17f1b649fb230
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 09:09:20 +0700

    accepted: solved with my own solution

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("This is (main) first python program for leetcode")
+
+from typing import List
+import copy
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ori = copy.deepcopy(heights)
+        heights.sort()
+        counter = 0
+        i = 0
+        for val in ori:
+            if val != heights[i]:
+                counter += 1
+            i += 1
+        return counter
+
+
+sol = Solution()
+print(sol.heightChecker([1, 2, 3, 4, 5]))
