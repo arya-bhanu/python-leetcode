f8743348ee67a65af56e553f384d523c2bf95e21
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-23 11:05:45 +0700

    accepted: with my own solution

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("This is (main) first python program for leetcode")
+from typing import List
+
+
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        counter = 0
+        for i in range(len(words)):
+            currWord = words[i]
+            j = i + 1
+            while (j < len(words)):
+                if self.isPrefixAndSuffix(currWord, words[j]):
+                    counter += 1
+                j += 1
+        return counter
+
+    def isPrefixAndSuffix(self, word1, word2):
+        if len(word1) > len(word2):
+            return False
+        pref = len(word2) - len(word1)
+        return True if word2[:len(
+            word1)] == word1 and word2[pref:] == word1 else False
+
+
+s = Solution()
+print(s.countPrefixSuffixPairs(["bb", "bb"]))
---
+++ try.py
@@ -0,0 +1,11 @@
+str = "abababa"
+str1 = "aba"
+pref = len(str) - len(str1)
+print(str[pref:])
+print(str[:len(str1)])
+
+
+j = 3
+while j < 4:
+    print("MASUK")
+    j += 1
