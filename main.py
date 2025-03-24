103d9baadc598a5ef0123da9e0ca3744e8542d44
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-24 20:25:46 +0700

    accepted: solved with --own solution

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from typing import List
+
+# use some kinds of permutation, find the possible iteration
+# use bruteforce
+# 3 loop of bruteforce
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        i = 0
+        counter = 0
+        while i < len(nums) - 2:
+            j = i + 1
+            while j < len(nums) - 1:
+                k = j + 1
+                while k < len(nums):
+                    if nums[j] - nums[i] == diff and nums[k] - nums[j] == diff:
+                        counter += 1
+                    k += 1
+                j += 1
+            i += 1
+        return counter
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(2, self.solution.arithmeticTriplets(
+            [0, 1, 4, 6, 7, 10], 3))
+        self.assertEqual(
+            2, self.solution.arithmeticTriplets([4, 5, 6, 7, 8, 9], 2))
 
 
 if __name__ == "__main__":
