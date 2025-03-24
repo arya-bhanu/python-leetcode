a51baed3344ad168e38f192afb0f40100901067b
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-24 20:05:26 +0700

    accepted: solved with own solution

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+from typing import List
 
 
+# naive approach
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def minimumAverage(self, nums: List[int]) -> float:
+        averages = []
+        nums.sort()
+        lastIndex = len(nums) - 1
+        for i in range(len(nums) // 2):
+            averages.append((nums[i] + nums[lastIndex - i]) / 2)
+        return min(averages)
--- test.py
+++ test.py
@@ -9,7 +9,11 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(5.5, self.solution.minimumAverage(
+            [7, 8, 3, 4, 15, 13, 4, 1]))
+        self.assertEqual(
+            5.5, self.solution.minimumAverage([1, 9, 8, 3, 10, 5]))
+        self.assertEqual(5.0, self.solution.minimumAverage([1, 2, 3, 7, 8, 9]))
 
 
 if __name__ == "__main__":
