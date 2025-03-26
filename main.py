e9f57d5e062ee410ee95011e96ba22dd4560ba3a
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-26 17:49:27 +0700

    accepted: with bruteforce

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from typing import List
+
+# naive use bruteforce
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(len(prices) - 1):
+            j = i + 1
+            for x in range(j, len(prices)):
+                if prices[x] <= prices[i]:
+                    prices[i] = prices[i] - prices[x]
+                    break
+        return prices
--- test.py
+++ test.py
@@ -9,7 +9,12 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [4, 2, 4, 2, 3], self.solution.finalPrices([8, 4, 6, 2, 3]))
+        self.assertEqual(
+            [1, 2, 3, 4, 5], self.solution.finalPrices([1, 2, 3, 4, 5]))
+        self.assertEqual(
+            [9, 0, 1, 6], self.solution.finalPrices([10, 1, 1, 6]))
 
 
 if __name__ == "__main__":
