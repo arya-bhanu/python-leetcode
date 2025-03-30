78dc1bc7a803915260f0dc171a23b7ef64507571
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-30 08:46:24 +0700

    accepted: with brute force my own solution

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from typing import List
 
 
+# naive, brute force approach
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        return [x for x in range(len(nums)) if nums[x] == target]
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [1, 2], self.solution.targetIndices([1, 2, 5, 2, 3], 2))
+        self.assertEqual([3], self.solution.targetIndices([1, 2, 5, 2, 3], 3))
+        self.assertEqual([4], self.solution.targetIndices([1, 2, 5, 2, 3], 5))
 
 
 if __name__ == "__main__":
