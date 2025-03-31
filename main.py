647761eedbad6d7f9be102d5083d8691cbd8fdc3
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-03-31 16:49:52 +0800

    not accepted: time limit exceded

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+        res = []
+        nums.sort()
+        for q in queries:
+            max = 0
+            for i in range(len(nums)):
+                if sum(nums[0:i+1]) <= q:
+                    temp = len(nums[0:i+1])
+                    if temp > max:
+                        max = temp
+            res.append(max)
+        return [0] if len(res) == 0 else res
--- test.py
+++ test.py
@@ -9,7 +9,11 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([2, 3, 4], self.solution.answerQueries(
+            [4, 5, 2, 1], [3, 10, 21]))
+        self.assertEqual([0], self.solution.answerQueries([2, 3, 4, 5], [1]))
+        self.assertEqual([1, 0, 0, 1, 1, 0, 0, 0], self.solution.answerQueries(
+            [624082], [972985, 564269, 607119, 693641, 787608, 46517, 500857, 140097]))
 
 
 if __name__ == "__main__":
