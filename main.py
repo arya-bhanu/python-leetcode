7fa930ea4c0a181e4b6bb0a7c527c9af603c2d42
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-01 10:42:50 +0800

    accepted: with my own solution

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from typing import List
+
+# solved
 
 
 class Solution:
-    def testMethod(self) -> int:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        k = len(nums) - 1
+        j = k - 1
+        i = k - 2
+        while i >= 0:
+            if nums[i] + nums[j] > nums[k]:
+                return nums[i] + nums[j] + nums[k]
+
+            i -= 1
+            j -= 1
+            k -= 1
         return 0
--- test.py
+++ test.py
@@ -9,7 +9,8 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(5, self.solution.largestPerimeter([2, 1, 2]))
+        self.assertEqual(0, self.solution.largestPerimeter([1, 2, 1, 10]))
 
 
 if __name__ == "__main__":
