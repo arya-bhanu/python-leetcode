a3098e06169722a416bc6c3147b1920d8dc403c2
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-03 19:37:25 +0700

    accepted: easy solved

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from typing import List
+
+# easy using bruteforce
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def isPrefixString(self, s: str, words: List[str]) -> bool:
+        targetLength = len(s)
+        conctStr = ""
+        for i in words:
+            conctStr += i
+            if len(conctStr) > targetLength:
+                return False
+            if len(conctStr) == targetLength and conctStr == s:
+                return True
+        return False
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(True, self.solution.isPrefixString(
+            "iloveleetcode", ["i", "love", "leetcode", "apples"]))
+        self.assertEqual(False, self.solution.isPrefixString(
+            "iloveleetcode", ["apples", "i", "love", "leetcode"]))
 
 
 if __name__ == "__main__":
