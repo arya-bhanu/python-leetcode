8117cd949de14e2272489b25ff1c0ac888e1dfb5
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-04 17:00:43 +0700

    accepted: slow --own

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+from typing import List
 
 
+# naive ways
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        res = []
+
+        def sumNext(i: int):
+            sum = 0
+            for i in range(i + 1, i + k + 1):
+                if i < len(code):
+                    sum += code[i]
+                else:
+                    sum += code[i - len(code)]
+            return sum
+
+        if k == 0:
+            return [0] * len(code)
+        if k > 0:
+            for i in range(len(code)):
+                res.append(sumNext(i))
+        else:
+            for i in range(len(code)):
+                sum = 0
+                for j in range(1, abs(k) + 1):
+                    sum += code[i - j]
+                res.append(sum)
+        return res
--- test.py
+++ test.py
@@ -9,7 +9,11 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([12, 10, 16, 13],
+                         self.solution.decrypt([5, 7, 1, 4], 3))
+        self.assertEqual([0, 0, 0, 0], self.solution.decrypt([1, 2, 3, 4], 0))
+        self.assertEqual(
+            [12, 5, 6, 13], self.solution.decrypt([2, 4, 9, 3], -2))
 
 
 if __name__ == "__main__":
