34a9da81c66f7b37951cc8b1ea51b38b293e7f7e
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-04 20:20:43 +0700

    accepted: with non efficient solution

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-
-
+# using bruteforce
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def countKConstraintSubstrings(self, s: str, k: int) -> int:
+        count_valid = 0
+        for i in range(len(s)):
+            j = i
+            counterOne, counterZero = 0, 0
+            while j < len(s):
+                if s[j] == "1":
+                    counterOne += 1
+                else:
+                    counterZero += 1
+                j += 1
+
+                if counterOne <= k or counterZero <= k:
+                    count_valid += 1
+
+        return count_valid
--- test.py
+++ test.py
@@ -9,7 +9,12 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            12, self.solution.countKConstraintSubstrings("10101", 1))
+        self.assertEqual(
+            25, self.solution.countKConstraintSubstrings("1010101", 2))
+        self.assertEqual(
+            15, self.solution.countKConstraintSubstrings("11111", 1))
 
 
 if __name__ == "__main__":
