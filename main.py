12f579148e7671e136039132162a0c3eda1be8f6
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-05 09:00:42 +0700

    accepted:  using sets --others solution

--- main.py
+++ main.py
@@ -10,29 +10,15 @@ from typing import List
 # 2 - x + y = 4
 
 
-# accepted using binary search
+# you can use sets for searching
+# so you dont have to sort first
 
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        aliceSizes.sort()
-        bobSizes.sort()
-
-        def bin_search(cand: int, arr: List[int]) -> bool:
-            i = 0
-            j = len(arr) - 1
-            while i <= j:
-                m = (i+j) // 2
-                if cand == arr[m]:
-                    return True
-                elif cand < arr[m]:
-                    j = m - 1
-                else:
-                    i = m + 1
-            return False
+        bobSets = set(bobSizes)
 
         for i in aliceSizes:
-            y = i + ((sum(bobSizes) - sum(aliceSizes)) // 2)
-            if y > 0 and bin_search(y, bobSizes):
+            y = i + (sum(bobSizes) - sum(aliceSizes)) // 2
+            if y in bobSets:
                 return [i, y]
-
         return []
