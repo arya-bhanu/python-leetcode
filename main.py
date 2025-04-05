0857f0485de4a14ac9396f418af61073423a11e0
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-05 15:02:52 +0700

    accepted: with neetcode

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from typing import Counter, List
+
+# solved
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        counts = Counter(nums)
+        for i in range(len(nums)):
+            size = len(nums) - i
+            if nums[i] < size:
+                continue
+            if nums[i] == size:
+                if i != 0 and nums[i] == nums[i-1]:
+                    continue
+                return nums[i]
+            if nums[i] > size:
+                between = 0 if i == 0 else nums[i-1]
+                if size > between and size < nums[i]:
+                    if i != 0 and nums[i] == nums[i-1]:
+                        continue
+                    return size
+        return -1
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(2, self.solution.specialArray([3, 5]))
+        self.assertEqual(-1, self.solution.specialArray([0, 0]))
+        self.assertEqual(3, self.solution.specialArray([0, 4, 3, 0, 4]))
+        self.assertEqual(-1, self.solution.specialArray([2, 3, 0, 2]))
 
 
 if __name__ == "__main__":
