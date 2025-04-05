08c8986e9c0ad9921f827a9f635e126bd795c7f5
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-04-05 08:53:39 +0700

    accepted: using own solution

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+from typing import List
 
+# alice - x + y = bob - y + x
+
+# 4 - x + y = 2 - y + x
+# 2y = 2x - 2
+# y = x - 1
+
+
+# 2 - x + y = 4
+
+
+# accepted using binary search
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        aliceSizes.sort()
+        bobSizes.sort()
+
+        def bin_search(cand: int, arr: List[int]) -> bool:
+            i = 0
+            j = len(arr) - 1
+            while i <= j:
+                m = (i+j) // 2
+                if cand == arr[m]:
+                    return True
+                elif cand < arr[m]:
+                    j = m - 1
+                else:
+                    i = m + 1
+            return False
+
+        for i in aliceSizes:
+            y = i + ((sum(bobSizes) - sum(aliceSizes)) // 2)
+            if y > 0 and bin_search(y, bobSizes):
+                return [i, y]
+
+        return []
--- test.py
+++ test.py
@@ -9,7 +9,11 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([1, 2], self.solution.fairCandySwap([1, 1], [2, 2]))
+        self.assertEqual([1, 2], self.solution.fairCandySwap([1, 2], [2, 3]))
+        self.assertEqual([2, 3], self.solution.fairCandySwap([2], [1, 3]))
+        self.assertEqual(
+            [5, 4], self.solution.fairCandySwap([1, 2, 5], [2, 4]))
 
 
 if __name__ == "__main__":
