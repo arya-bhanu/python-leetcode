2cc57b72da3d950ba0254b26e7d15093fb39ef42
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-15 08:39:25 +0700

    feat: solved with monolithic stack

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from typing import List
 
-# naive ways using dictionary
-# naive using bruteforce
+# using monolithic stack
 
 
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         dict = {}
         result = [-1] * len(nums1)
+        # stores location of each nums1
         for i in range(len(nums1)):
             dict[nums1[i]] = i
-        for i in range(len(nums2) - 1):
-            if nums2[i] not in dict:
-                continue
-            for x in range(i + 1, len(nums2)):
-                if nums2[x] > nums2[i]:
-                    result[dict[nums2[i]]] = nums2[x]
-                    break
+
+        # monolithic stack algorithm
+        stack = []
+        for i in nums2:
+            # we need to peek first in while loop
+            while (len(stack) > 0 and stack[-1] < i):
+                val = stack[-1]
+                result[dict[val]] = i
+                stack.pop()
+            if i in dict:
+                stack.append(i)
+
         return result
