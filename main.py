14f20ecc82974dd2474f391e2dbce5dc497b2c03
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-15 08:02:14 +0700

    feat: solved with naive solution --someone

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from typing import List
+
+# naive ways using dictionary
+# naive using bruteforce
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dict = {}
+        result = [-1] * len(nums1)
+        for i in range(len(nums1)):
+            dict[nums1[i]] = i
+        for i in range(len(nums2) - 1):
+            if nums2[i] not in dict:
+                continue
+            for x in range(i + 1, len(nums2)):
+                if nums2[x] > nums2[i]:
+                    result[dict[nums2[i]]] = nums2[x]
+                    break
+        return result
--- test.py
+++ test.py
@@ -9,7 +9,12 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [-1, 3, -1], self.solution.nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
+        self.assertEqual(
+            [3, -1], self.solution.nextGreaterElement([2, 4], [1, 2, 3, 4]))
+        self.assertEqual([7, 7, 7, 7, 7], self.solution.nextGreaterElement(
+            [1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
 
 
 if __name__ == "__main__":
