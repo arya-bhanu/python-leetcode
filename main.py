8ad6d2d9296df991fc70e64659a13b73626596a6
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-16 08:34:29 +0700

    feat: solved with my own solution

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from typing import List
+from collections import OrderedDict
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def mergeArrays(self, x1: List[List[int]], x2: List[List[int]]) -> List[List[int]]:
+        dict = {}
+        res = []
+        if len(x1) < len(x2):
+            nums2 = x2
+            nums1 = x1
+        elif len(x2) <= len(x1):
+            nums2 = x1
+            nums1 = x2
+        for i in range(len(nums2)):
+            if i < len(nums1):
+                dict[nums2[i][0]] = dict.get(nums2[i][0], 0) + nums2[i][1]
+                dict[nums1[i][0]] = dict.get(nums1[i][0], 0) + nums1[i][1]
+            else:
+                dict[nums2[i][0]] = dict.get(nums2[i][0], 0) + nums2[i][1]
+        for key, val in OrderedDict(sorted(dict.items())).items():
+            res.append([key, val])
+        return res
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([[1, 6], [2, 3], [3, 2], [4, 6]], self.solution.mergeArrays(
+            [[1, 2], [2, 3], [4, 5]], [[1, 4], [3, 2], [4, 1]]))
+        self.assertEqual([[1, 3], [2, 4], [3, 6], [4, 3], [5, 5]], self.solution.mergeArrays(
+            [[2, 4], [3, 6], [5, 5]], [[1, 3], [4, 3]]))
 
 
 if __name__ == "__main__":
