1e10450a9ba7d29c226a56f070a53eef75bd9c32
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-17 13:19:11 +0700

    solved: with my own solution

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from typing import List
+
+# accepted but very slow
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        def isEven(num: int):
+            return num % 2 == 0
+        for i in range(len(nums)):
+            if isEven(i):
+                if isEven(nums[i]):
+                    continue
+                else:
+                    j = i + 1
+                    while j < len(nums):
+                        if isEven(nums[j]):
+                            temp = nums[j]
+                            nums[j] = nums[i]
+                            nums[i] = temp
+                            break
+                        j += 1
+            else:
+                if not isEven(nums[i]):
+                    continue
+                else:
+                    j = i + 1
+                    while j < len(nums):
+                        if not isEven(nums[j]):
+                            temp = nums[j]
+                            nums[j] = nums[i]
+                            nums[i] = temp
+                            break
+                        j += 1
+        return nums
--- test.py
+++ test.py
@@ -9,7 +9,9 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [4, 5, 2, 7], self.solution.sortArrayByParityII([4, 2, 5, 7]))
+        self.assertEqual([2, 3], self.solution.sortArrayByParityII([2, 3]))
 
 
 if __name__ == "__main__":
