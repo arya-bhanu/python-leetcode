cd93eae57cd8324d0f874c22625af9449a06de9b
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-18 15:41:36 +0700

    feat: solved with my own solution

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from typing import List
+
+# naive iteration
+# very slow
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        dist = []
+        res = []
+        for i in range(len(s)):
+            if s[i] == c:
+                dist.append(i)
+        for i in range(len(s)):
+            min = -1
+            for j in dist:
+                absVal = abs(j - i)
+                if min == -1:
+                    min = absVal
+                if absVal < min:
+                    min = absVal
+            res.append(min)
+        return res
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0],
+                         self.solution.shortestToChar("loveleetcode", "e"))
+        self.assertEqual(
+            [3, 2, 1, 0], self.solution.shortestToChar("aaab", "b"))
 
 
 if __name__ == "__main__":
