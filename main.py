f2387ad5ac8e4d0c1ba9cffbd04c3da091591d91
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-19 17:58:33 +0700

    feat: solved with others solution

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from typing import List
 
 
-# very easy, use more memory
+# using two pointer
+# also slow?
 
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        left = []
-        right = []
-        mid = []
-        for num in nums:
-            if num < pivot:
-                left.append(num)
-            elif num == pivot:
-                mid.append(num)
-            else:
-                right.append(num)
-        return left + mid + right
+        result = [0] * len(nums)
+        length = len(nums)
+        left = 0
+        right = length - 1
+
+        for i in range(len(nums)):
+            j = length - 1 - i
+            if nums[i] < pivot:
+                result[left] = nums[i]
+                left += 1
+            if nums[j] > pivot:
+                result[right] = nums[j]
+                right -= 1
+        while left <= right:
+            result[left] = pivot
+            left += 1
+        return result
