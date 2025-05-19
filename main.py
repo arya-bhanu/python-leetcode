3ea1808c24ee6b457da12546f8d2bcf8b2c2c102
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-19 17:11:38 +0700

    feat: solved with others solution

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from typing import List
 
-# naive iteration
-# very slow
+# others solution
 
 
 class Solution:
-    def shortestToChar(self, s: str, c: str) -> List[int]:
-        dist = []
-        res = []
+    def shortestToChar(self, s: str, c: str) -> list[int]:
+        c_index_in_s = []
+
+        # Create a list of index c in s
         for i in range(len(s)):
             if s[i] == c:
-                dist.append(i)
+                c_index_in_s.append(i)
+
+        answer = []
+        c_index = 0
         for i in range(len(s)):
-            min = -1
-            for j in dist:
-                absVal = abs(j - i)
-                if min == -1:
-                    min = absVal
-                if absVal < min:
-                    min = absVal
-            res.append(min)
-        return res
+            # if it is c add 0
+            if s[i] == c:
+                answer.append(0)
+                c_index += 1
+            # If it is first element of c in s
+            elif i < c_index_in_s[0]:
+                answer.append(c_index_in_s[0] - i)
+            # If it is last element of c in s
+            elif i > c_index_in_s[-1]:
+                answer.append(i - c_index_in_s[-1])
+            # If it is in the middle use minimum
+            else:
+                # find minimum distance between i is before value of current c index and i after 1 before current c index
+                answer.append(
+                    min((c_index_in_s[c_index] - i), (i - c_index_in_s[c_index - 1])))
+        return answer
