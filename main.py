cce72bdcff2a9143910e2cb0570f8e55aaa153eb
arya-bhanuartha <aryacreative.dev@gmail.com>
2025-05-25 21:31:46 +0700

    feat: solved with others solution, must understand recursion first

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i: int):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # left node, will run first until base case
+            subset.append(nums[i])
+            dfs(i + 1)
+            # right node
+            subset.pop()
+            dfs(i + 1)
+        dfs(0)
+        return res
--- test.py
+++ test.py
@@ -9,7 +9,15 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            sorted([[], [1], [2], [1, 2], [3], [1, 3],
+                   [2, 3], [1, 2, 3]], key=sorted),
+            sorted(self.solution.subsets([1, 2, 3]), key=sorted)
+        )
+        self.assertEqual(
+            sorted([[], [0]], key=sorted),
+            sorted(self.solution.subsets([0]), key=sorted)
+        )
 
 
 if __name__ == "__main__":
