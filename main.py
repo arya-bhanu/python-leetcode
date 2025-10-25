1d35268a97d7b49b28be771f8e954e6d51855260
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-10-25 09:15:57 +0700

    feat: not fastest

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
+        sets = set()
+        o = []
+        for i in friends:
+            sets.add(i)
+        for ord in order:
+            if ord in sets:
+                o.append(ord)
+        return o
--- test.py
+++ test.py
@@ -9,7 +9,10 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [3, 1, 4], self.solution.recoverOrder([3, 1, 2, 5, 4], [1, 3, 4])
+        )
+        self.assertEqual([5, 2], self.solution.recoverOrder([1, 4, 5, 3, 2], [2, 5]))
 
 
 if __name__ == "__main__":
