048159582f4d125e28bea73b1e4afeb9882d191d
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-10-25 09:06:10 +0700

    feat: need to check

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def subarraySum(self, nums: List[int]) -> int:
+        sum = 0
+        for i in range(len(nums)):
+            start = max(0, i - nums[i])
+            counted = self.counterSum(start, i, nums)
+            sum += counted
+        return sum
+
+    def counterSum(self, start: int, end: int, nums):
+        sum = 0
+
+        while start <= end:
+            sum += nums[start]
+            start += 1
+
+        return sum
--- test.py
+++ test.py
@@ -9,7 +9,8 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(11, self.solution.subarraySum([2, 3, 1]))
+        self.assertEqual(13, self.solution.subarraySum([3, 1, 1, 2]))
 
 
 if __name__ == "__main__":
