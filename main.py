a3accaf7b011a9d0d5c3b1e61e54a19262568cc5
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-10-25 07:15:10 +0700

    faet: done fastest

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        ops = {"++X": 1, "X++": 1, "--X": -1, "X--": -1}
+        val = 0
+        for opr in operations:
+            val += ops[opr]
+        return val
--- test.py
+++ test.py
@@ -9,7 +9,15 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            1, self.solution.finalValueAfterOperations(["--X", "X++", "X++"])
+        )
+        self.assertEqual(
+            3, self.solution.finalValueAfterOperations(["++X", "++X", "X++"])
+        )
+        self.assertEqual(
+            0, self.solution.finalValueAfterOperations(["X++", "++X", "--X", "X--"])
+        )
 
 
 if __name__ == "__main__":
