4e3f9e91ab97286719963d8a7f173f1850540439
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-10-28 06:58:56 +0700

    feat: max 100%

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-
-
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def convertDateToBinary(self, date: str) -> str:
+        splitted = date.split("-")
+        return self.convertIntoBinaryString(splitted)
+
+    def convertIntoBinaryString(self, strArr: list[str]):
+        resultArr = []
+        for s in strArr:
+            num = int(s)
+            res = num
+            resultStr = []
+            while res != 0:
+                rem = res % 2
+                res = res // 2
+                resultStr.insert(0, rem)
+            resultArr.append("".join(map(str, resultStr)))
+        return "-".join(resultArr)
--- test.py
+++ test.py
@@ -9,7 +9,12 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            "100000100000-10-11101", self.solution.convertDateToBinary("2080-02-29")
+        )
+        self.assertEqual(
+            "11101101100-1-1", self.solution.convertDateToBinary("1900-01-01")
+        )
 
 
 if __name__ == "__main__":
