ba708e84899e953fc22f8bab4cde79cf2cbfe5ee
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-10-30 08:35:20 +0700

    feat: very slow

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def duplicateZeros(self, arr: List[int]):
+        pIndex = 0
+        while pIndex < len(arr) - 1:
+            if arr[pIndex] == 0:
+                i = pIndex
+                a = -1
+                while i < len(arr):
+                    tempA = arr[i]
+                    if a > -1:
+                        arr[i] = a
+                    a = tempA
+                    i += 1
+                pIndex += 2
+            else:
+                pIndex += 1
