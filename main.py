9e4e016d1bb1455e5f5700bbf6bc2075191fba81
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-02 18:59:20 +0700

    pretty fast

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        maps = {}
+        raws = nums[:]
+        nums.sort()
+        result = []
+
+        for raw in raws:
+            if raw in maps:
+                result.append(maps[raw])
+                continue
+            counter = 0
+            j = 0
+            while nums[j] < raw and j < len(raws):
+                counter += 1
+                j += 1
+            maps[raw] = counter
+            result.append(counter)
+        return result
--- test.py
+++ test.py
@@ -9,7 +9,15 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [4, 0, 1, 1, 3], self.solution.smallerNumbersThanCurrent([8, 1, 2, 2, 3])
+        )
+        self.assertEqual(
+            [2, 1, 0, 3], self.solution.smallerNumbersThanCurrent([6, 5, 4, 8])
+        )
+        self.assertEqual(
+            [0, 0, 0, 0], self.solution.smallerNumbersThanCurrent([7, 7, 7, 7])
+        )
 
 
 if __name__ == "__main__":
