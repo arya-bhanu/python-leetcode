d5fb72e3f3c28e417cff733e69911d82cc1cb843
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-04 22:54:05 +0700

    using hashmap (dynamic programming)

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from typing import List
 class Solution:
     # greedy solution
     def countKDifference(self, nums: List[int], k: int) -> int:
+        # using hashmap
+        hash_map = {}
         counter = 0
-        i = 0
-        while i < len(nums) - 1:
-            j = i + 1
-            while j < len(nums):
-                if abs(nums[i] - nums[j]) == k:
-                    counter += 1
-                j += 1
-            i += 1
+        for num in nums:
+            if num not in hash_map:
+                hash_map[num] = 1
+                continue
+            curr = hash_map[num]
+            curr += 1
+            hash_map[num] = curr
+        for hash_num in hash_map:
+            if hash_num - k in hash_map:
+                counter += hash_map[hash_num] * hash_map[hash_num - k]
         return counter
