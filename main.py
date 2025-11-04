4060a576a5aa8112eb0cf2c64c818b1933c85401
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-05 06:55:22 +0700

    done ez

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def diStringMatch(self, s: str) -> List[int]:
+        max = len(s)
+        min = 0
+        res = []
+        for schr in s:
+            if schr == "I":
+                res.append(min)
+                min += 1
+                continue
+            if schr == "D":
+                res.append(max)
+                max -= 1
+        if s[len(s) - 1] == "D":
+            res.append(min)
+        if s[len(s) - 1] == "I":
+            res.append(max)
+        return res
--- test.py
+++ test.py
@@ -9,7 +9,9 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual([0, 4, 1, 3, 2], self.solution.diStringMatch("IDID"))
+        self.assertEqual([0, 1, 2, 3], self.solution.diStringMatch("III"))
+        self.assertEqual([3, 2, 0, 1], self.solution.diStringMatch("DDI"))
 
 
 if __name__ == "__main__":
