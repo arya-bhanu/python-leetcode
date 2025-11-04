71b1df27fbae72bc843b3009b4a27d3a0cf1a188
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-04 22:46:52 +0700

    feat: greedy solution

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    # greedy solution
+    def countKDifference(self, nums: List[int], k: int) -> int:
+        counter = 0
+        i = 0
+        while i < len(nums) - 1:
+            j = i + 1
+            while j < len(nums):
+                if abs(nums[i] - nums[j]) == k:
+                    counter += 1
+                j += 1
+            i += 1
+        return counter
--- test.py
+++ test.py
@@ -9,7 +9,9 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(4, self.solution.countKDifference([1, 2, 2, 1], 1))
+        self.assertEqual(0, self.solution.countKDifference([1, 3], 3))
+        self.assertEqual(3, self.solution.countKDifference([3, 2, 1, 5, 4], 2))
 
 
 if __name__ == "__main__":
