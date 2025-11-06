83e51e42e2f343bdabbdd8480b96ba3217d38e92
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-06 07:17:06 +0700

    fast

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i_nums1 = 0
+        i_nums2 = 0
+        set_nums_1 = set(nums1)
+        set_nums_2 = set(nums2)
+        for num in nums1:
+            if num in set_nums_2:
+                i_nums1 += 1
+        for num in nums2:
+            if num in set_nums_1:
+                i_nums2 += 1
+        return [i_nums1, i_nums2]
--- test.py
+++ test.py
@@ -9,7 +9,22 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [2, 1], self.solution.findIntersectionValues([2, 3, 2], [1, 2])
+        )
+        self.assertEqual(
+            [3, 4],
+            self.solution.findIntersectionValues([4, 3, 2, 3, 1], [2, 2, 5, 2, 3, 6]),
+        )
+        self.assertEqual(
+            [0, 0], self.solution.findIntersectionValues([3, 4, 2, 3], [1, 5])
+        )
+        self.assertEqual(
+            [4, 2],
+            self.solution.findIntersectionValues(
+                [24, 28, 7, 27, 7, 27, 9, 24, 9, 10], [12, 29, 9, 7, 5]
+            ),
+        )
 
 
 if __name__ == "__main__":
