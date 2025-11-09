44493d4fc4804c6cb6d29cbb9cfd9025da3eee96
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-09 12:19:25 +0700

    anjir bisa cok 1x attempt wkwkwk

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+from typing import List
+import math
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        dicts = {}
+        for i in range(len(groupSizes)):
+            if groupSizes[i] not in dicts:
+                arr = []
+                arr.append(i)
+                dicts[groupSizes[i]] = arr
+                continue
+            curr = dicts[groupSizes[i]]
+            curr.append(i)
+            dicts[groupSizes[i]] = curr
+        result = []
+        for key in dicts:
+            if len(dicts[key]) > key:
+                length_total = len(dicts[key])
+                loop = math.ceil(length_total / key)
+                for i in range(loop):
+                    if length_total >= key:
+                        result.append(dicts[key][i * key : (i * key + key)])
+                        length_total -= key
+                        continue
+                    result.append(
+                        dicts[key][len(dicts[key]) - length_total : len(dicts[key])]
+                    )
+                continue
+            result.append(dicts[key])
+        return result
+
+
+sol = Solution()
+print(sol.groupThePeople([2, 1, 3, 3, 3, 3, 3, 3, 3, 2]))
--- test.py
+++ test.py
@@ -9,7 +9,8 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(0, self.solution.groupThePeople())
+        self.assertEqual(0, self.solution.groupThePeople())
 
 
 if __name__ == "__main__":
