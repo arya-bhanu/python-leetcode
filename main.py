5057b4e0afcb257ccbe7fe8d6edea8419183c0d0
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-11-09 10:34:39 +0700

    fastest anying

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from typing import List
 
 
 class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        # greedy
+        repeated = -1
+        missing = -1
+        length_grid = len(grid)
+        length = length_grid * length_grid
+        dicts = {}
+        for row in grid:
+            for col in row:
+                if col not in dicts:
+                    dicts[col] = 1
+                    continue
+                curr = dicts[col]
+                curr += 1
+                dicts[col] = curr
+                repeated = col
+        for i in range(1, length + 1):
+            if i not in dicts:
+                missing = i
+        return [repeated, missing]
--- test.py
+++ test.py
@@ -9,7 +9,15 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(0, self.solution.testMethod())
+        self.assertEqual(
+            [2, 4], self.solution.findMissingAndRepeatedValues([[1, 3], [2, 2]])
+        )
+        self.assertEqual(
+            [9, 5],
+            self.solution.findMissingAndRepeatedValues(
+                [[9, 1, 7], [8, 9, 2], [3, 4, 6]]
+            ),
+        )
 
 
 if __name__ == "__main__":
