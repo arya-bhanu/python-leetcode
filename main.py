348406cbe2b623a85790021a44002ed3a78f0e63
putu-bhanuartha <putu.bhanuartha@paper.id>
2026-04-30 06:02:27 +0700

    feat: solve own

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+class MyHashSet:
+    def __init__(self):
+        self.sets = set()
+        return
 
+    def add(self, key: int) -> None:
+        self.sets.add(key)
 
-class Solution:
-    def testMethod(self) -> int:
-        return 0
+    def remove(self, key: int) -> None:
+        self.sets.discard(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.sets
