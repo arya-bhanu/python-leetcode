d04b3e5baf83c77476990a39bad12a3074523002
Putu Bhanuartha <aryacreative.dev@gmail.com>
2026-06-09 05:46:33 +0700

    feat: solved - always check each element in array A and B in set of seen - if current element present in seen set, then always count it's presence, sum it with previous count

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ from typing import List
 
 
 class Solution:
-    def findLHS(self, nums: List[int]) -> int:
-        # loop and count all numbers in array nums store into dictionary
-        # loop dictionary to find the count of number + count of successor number, find the max
-        return
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        seen = set()
+        result = []
+        if len(A) == len(B):
+            counter = 0
+            prev = 0
+            for i, a in enumerate(A):
+                if a in seen:
+                    counter = counter + 1
+                else:
+                    seen.add(a)
+
+                if B[i] in seen:
+                    counter = counter + 1
+                else:
+                    seen.add(B[i])
+                if len(result) > 0:
+                    prev = result[-1]
+                result.append(prev + counter)
+                counter = 0
+        return result
--- test.py
+++ test.py
@@ -9,9 +9,13 @@ class TestSolution(unittest.TestCase):
         return super().setUp()
 
     def test_start(self):
-        self.assertEqual(5, self.solution.findLHS([1, 3, 2, 2, 5, 2, 3, 7]))
-        self.assertEqual(2, self.solution.findLHS([1, 2, 3, 4]))
-        self.assertEqual(0, self.solution.findLHS([1, 1, 1, 1]))
+        self.assertEqual(
+            [0, 2, 3, 4],
+            self.solution.findThePrefixCommonArray([1, 3, 2, 4], [3, 1, 2, 4]),
+        )
+        self.assertEqual(
+            [0, 1, 3], self.solution.findThePrefixCommonArray([2, 3, 1], [3, 1, 2])
+        )
 
 
 if __name__ == "__main__":
