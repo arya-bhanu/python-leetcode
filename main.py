a75b9f7d2a82bc6da6b1845e97501484de9832db
Putu Bhanuartha <aryacreative.dev@gmail.com>
2026-06-08 20:43:48 +0700

    feat: solved self

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from typing import List
+import base64
 
 
-class Solution:
-    def findLHS(self, nums: List[int]) -> int:
-        # loop and count all numbers in array nums store into dictionary
-        # loop dictionary to find the count of number + count of successor number, find the max
-        return
+class Codec:
+
+    def __init__(self):
+        self.dict = {}
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL."""
+        encoded = base64.b64encode(longUrl.encode("ascii")).decode("ascii")
+        self.dict[encoded] = longUrl
+        return encoded
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL."""
+        if shortUrl in self.dict:
+            return self.dict[shortUrl]
+
+        return base64.b64decode(shortUrl.encode("ascii")).decode("ascii")
+
+
+# Your Codec object will be instantiated and called as such:
+url = "https://leetcode.com/problems/design-tinyurl"
+codec = Codec()
+encoded = codec.encode(url)
+print(encoded)
+print(codec.decode(encoded))
